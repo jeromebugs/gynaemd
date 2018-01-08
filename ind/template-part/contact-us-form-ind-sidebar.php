efc8e7b4fbe9f223ccaf6580b7e0c782b07cf65c
activa-juno <35136011+activa-juno@users.noreply.github.com>
2018-01-08 16:48:27 +0800

    Initial release

---
+++ ind/template-part/contact-us-form-ind-sidebar.php
@@ -0,0 +1,14 @@
+<div class="widget">
+	<div class="textwidget">
+		<div class="sidebar-request-an-appointment-wrapper">
+			<div class="request-an-appointment-form-header widget-title">
+				Permintaan Sebuah Penunjukan
+			</div>
+			<div class="request-an-appointment-form-content">
+				<!-- feedback.activamedia.com.sg script begins here -->
+				<script type="text/javascript" defer src="//feedback.activamedia.com.sg/embed/3058567.js" data-role="form" data-default-width="650px"></script>
+				<!-- feedback.activamedia.com.sg script ends here -->
+			</div>
+		</div>
+	</div>
+</div>
---
+++ media.php
@@ -0,0 +1,386 @@
+<?php include('functions.php') ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="dist/icon/favicon.png" rel="icon" type="img/png">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+<title>Media | GynaeMD Women's & Rejuvenation Clinic</title>
+<meta name="description" content="Got a question about pregnancy or gynaecology services? We have a collection of obstetrics & gynaecology articles that are here for your reading."/>
+
+<?php include('template-part/styles.php') ?>
+
+<link rel="canonical" href="<?php echo lang_homepage_url() ?>media.html" />
+
+<link rel="alternate" href="<?php echo lang_homepage_url() ?>media.html" hreflang="x-default">
+<link rel="alternate" href="<?php echo lang_homepage_url() ?>media.html" hreflang="en">
+
+<style type="text/css">
+	@font-face {
+		font-family: 'Futura';
+		src: url('<?php echo homepage_url() ?>dist/fonts/Futura/FuturaBT-Light.eot');
+		src: url('<?php echo homepage_url() ?>dist/fonts/Futura/FuturaBT-Light.eot?#iefix') format('embedded-opentype'),
+			url('<?php echo homepage_url() ?>dist/fonts/Futura/FuturaBT-Light.woff2') format('woff2'),
+			url('<?php echo homepage_url() ?>dist/fonts/Futura/FuturaBT-Light.woff') format('woff'),
+			url('<?php echo homepage_url() ?>dist/fonts/Futura/FuturaBT-Light.ttf') format('truetype'),
+			url('<?php echo homepage_url() ?>dist/fonts/Futura/FuturaBT-Light.svg#FuturaBT-Light') format('svg');
+		font-weight: 300;
+		font-style: normal;
+	}
+</style>
+
+</head>
+
+<body>
+
+<!-- HEADER CONTAINER -->
+<div id="header" class="en-lang">
+
+	<div class="header-inner">
+		
+		<div class="top-language-switcher">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<?php include('template-part/language-switcher.php') ?>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="container">
+
+			<div class="upper-header">
+				<div class="navbar-fix">
+
+					<a href="<?php echo homepage_url() ?>" class="logo-link">
+						<img src="<?php echo homepage_url() ?>dist/images/logo.png" class="img-responsive">
+					</a>
+
+					<div class="clearfix"></div>
+
+					<div class="nav-header-wrapper">
+						<div class="nav-header">
+						
+							<?php include('template-part/navigation.php') ?>
+
+						</div>
+					</div>
+
+				</div>
+
+			</div>
+
+		</div>
+
+		<div class="container">
+			<div class="banner-wrapper" style="">
+				<img src="<?php echo homepage_url() ?>dist/images/bottom_flower_vines.png" class="flower-vines">
+				
+				<img src="<?php echo homepage_url() ?>dist/images/banner-1.jpg" class="img-responsive img-full desktop-banner hidden-sm hidden-xs">
+				<img src="<?php echo homepage_url() ?>dist/images/banner-1-mobile.jpg" class="img-responsive img-full mobile-banner hidden-md hidden-lg">
+			</div>
+		</div>
+		
+	</div>
+	
+</div>
+<!-- END HEADER CONTAINER -->
+
+
+<!-- PAGE CONTENT CONTAINER -->
+<div id="page-content-wrapper">
+	<div class="container">
+		<div class="row">
+			
+			<div class="col-lg-8 col-md-8 col-sm-12 col-xs-12 col-md-push-4">
+
+				<!-- breadcrumbs -->
+				<div class="breadcrumbs">
+					<p id="breadcrumbs">You are here:
+						<span xmlns:v="http://rdf.data-vocabulary.org/#">
+							<span typeof="v:Breadcrumb">
+								<a href="<?php echo homepage_url() ?>" rel="v:url" property="v:title">Home</a> » 
+								<span class="breadcrumb_last">Media</span>
+							</span>
+						</span>
+					</p>
+				</div>
+				<!-- end .breadcrumbs -->
+
+				<!-- page-title -->
+				<h1 class="page-title">MEDIA</h1>
+				<!-- end .page-title -->
+				
+				<!-- .pdf-grid -->
+				<div class="pdf-grid">
+					<div class="row">
+                        <div class="col-lg-4 col-md-6 col-sm-6">
+							<a href="<?php echo homepage_url() ?>pdf/motherhood-october-2017.pdf" target="_blank">
+								<div class="pdf-item fix-height" data-fix-height-group="pdf-group" style="height: 386px;">
+									<div class="pdf-thumbnail">
+										<img src="<?php echo homepage_url() ?>dist/images/motherhood-october-2017.png" class="img-responsive">
+									</div>
+									<div class="pdf-title">
+										Motherhood October 2017											
+									</div>
+								</div>
+							</a>
+						</div>
+						<div class="col-lg-4 col-md-6 col-sm-6">
+							<a href="<?php echo homepage_url() ?>pdf/expat-living-city-guide-pregnancy-2017.pdf" target="_blank">
+								<div class="pdf-item fix-height" data-fix-height-group="pdf-group" style="height: 386px;">
+									<div class="pdf-thumbnail">
+										<img src="<?php echo homepage_url() ?>dist/images/expat-living-city-guide-pregnancy-2017.png" class="img-responsive">
+									</div>
+									<div class="pdf-title">
+										Expat Living City Guide 2017											
+									</div>
+								</div>
+							</a>
+						</div>
+						<div class="col-lg-4 col-md-6 col-sm-6">
+							<a href="<?php echo homepage_url() ?>pdf/her-world-july-2017.pdf" target="_blank">
+								<div class="pdf-item fix-height" data-fix-height-group="pdf-group" style="height: 386px;">
+									<div class="pdf-thumbnail">
+										<img src="<?php echo homepage_url() ?>dist/images/her-world-july-2017.png" class="img-responsive">
+									</div>
+									<div class="pdf-title">
+										Her World July 2017											
+									</div>
+								</div>
+							</a>
+						</div>
+						<div class="col-lg-4 col-md-6 col-sm-6">
+							<a href="<?php echo homepage_url() ?>pdf/motherhood-complications-in-labour.pdf" target="_blank">
+								<div class="pdf-item fix-height" data-fix-height-group="pdf-group" style="height: 386px;">
+									<div class="pdf-thumbnail">
+										<img src="<?php echo homepage_url() ?>dist/images/motherhood-complications-in-labour.png" class="img-responsive">
+									</div>
+									<div class="pdf-title">
+										Motherhood May 2017											
+									</div>
+								</div>
+							</a>
+						</div>
+						<div class="col-lg-4 col-md-6 col-sm-6">
+							<a href="<?php echo homepage_url() ?>pdf/motherhood-pregnancy-ultrasound.pdf" target="_blank">
+								<div class="pdf-item fix-height" data-fix-height-group="pdf-group" style="height: 386px;">
+									<div class="pdf-thumbnail">
+										<img src="<?php echo homepage_url() ?>dist/images/motherhood-mar17.png" class="img-responsive">
+									</div>
+									<div class="pdf-title">
+										Motherhood March 2017											
+									</div>
+								</div>
+							</a>
+						</div>
+						<div class="col-lg-4 col-md-6 col-sm-6">
+							<a href="<?php echo homepage_url() ?>pdf/young-parents-jan-17.pdf" target="_blank">
+								<div class="pdf-item fix-height" data-fix-height-group="pdf-group" style="height: 386px;">
+									<div class="pdf-thumbnail">
+										<img src="<?php echo homepage_url() ?>dist/images/young-parents-jan-17.png" class="img-responsive">
+									</div>
+									<div class="pdf-title">
+										Young Parents January 2017											
+									</div>
+								</div>
+							</a>
+						</div>
+						<div class="col-lg-4 col-md-6 col-sm-6">
+							<a href="<?php echo homepage_url() ?>pdf/motherhood-sept-2016.pdf" target="_blank">
+								<div class="pdf-item fix-height" data-fix-height-group="pdf-group" style="height: 386px;">
+									<div class="pdf-thumbnail">
+										<img src="<?php echo homepage_url() ?>dist/images/motherhood-sept-2016.png" class="img-responsive">
+									</div>
+									<div class="pdf-title">
+										Motherhood September 2016											
+									</div>
+								</div>
+							</a>
+						</div>
+						<div class="col-lg-4 col-md-6 col-sm-6">
+							<a href="<?php echo homepage_url() ?>pdf/mims-doctor-sg.pdf" target="_blank">
+								<div class="pdf-item fix-height" data-fix-height-group="pdf-group" style="height: 386px;">
+									<div class="pdf-thumbnail">
+										<img src="<?php echo homepage_url() ?>dist/images/mims-doctor-sg.png" class="img-responsive">
+									</div>
+									<div class="pdf-title">
+										MIMS Doctor SG September 2016											
+									</div>
+								</div>
+							</a>
+						</div>
+						<div class="col-lg-4 col-md-6 col-sm-6">
+							<a href="<?php echo homepage_url() ?>pdf/expat-living-body-and-mind-guide-2016.pdf" target="_blank">
+								<div class="pdf-item fix-height" data-fix-height-group="pdf-group" style="height: 386px;">
+									<div class="pdf-thumbnail">
+										<img src="<?php echo homepage_url() ?>dist/images/expat-living-body-and-mind-guide-2016.png" class="img-responsive">
+									</div>
+									<div class="pdf-title">
+										Expat Living May 2016											
+									</div>
+								</div>
+							</a>
+						</div>
+						<div class="col-lg-4 col-md-6 col-sm-6">
+							<a href="<?php echo homepage_url() ?>pdf/herw-world-may16.pdf" target="_blank">
+								<div class="pdf-item fix-height" data-fix-height-group="pdf-group" style="height: 386px;">
+									<div class="pdf-thumbnail">
+										<img src="<?php echo homepage_url() ?>dist/images/herw-world-may16.png" class="img-responsive">
+									</div>
+									<div class="pdf-title">
+										Her World May 2016											
+									</div>
+								</div>
+							</a>
+						</div>
+						<div class="col-lg-4 col-md-6 col-sm-6">
+							<a href="<?php echo homepage_url() ?>pdf/simply-her-jan-16.pdf" target="_blank">
+								<div class="pdf-item fix-height" data-fix-height-group="pdf-group" style="height: 386px;">
+									<div class="pdf-thumbnail">
+										<img src="<?php echo homepage_url() ?>dist/images/simply-her-jan-16.png" class="img-responsive">
+									</div>
+									<div class="pdf-title">
+										Simply Her January 2016											
+									</div>
+								</div>
+							</a>
+						</div>
+						<div class="col-lg-4 col-md-6 col-sm-6">
+							<a href="<?php echo homepage_url() ?>pdf/expat-living-city-guide-2016.pdf" target="_blank">
+								<div class="pdf-item fix-height" data-fix-height-group="pdf-group" style="height: 386px;">
+									<div class="pdf-thumbnail">
+										<img src="<?php echo homepage_url() ?>dist/images/expat-living-city-guide-2016.png" class="img-responsive">
+									</div>
+									<div class="pdf-title">
+										Expat Living City Guide 2016											
+									</div>
+								</div>
+							</a>
+						</div>
+						<div class="col-lg-4 col-md-6 col-sm-6">
+							<a href="<?php echo homepage_url() ?>pdf/singapore-child-magazine-november-2015.pdf" target="_blank">
+								<div class="pdf-item fix-height" data-fix-height-group="pdf-group" style="height: 386px;">
+									<div class="pdf-thumbnail">
+										<img src="<?php echo homepage_url() ?>dist/images/1233.png" class="img-responsive">
+									</div>
+									<div class="pdf-title">
+										Singapore's Child Magazine November 2015											
+									</div>
+								</div>
+							</a>
+						</div>
+						<div class="col-lg-4 col-md-6 col-sm-6">
+							<a href="<?php echo homepage_url() ?>pdf/parents-world-sep-oct2015.pdf" target="_blank">
+								<div class="pdf-item fix-height" data-fix-height-group="pdf-group" style="height: 386px;">
+									<div class="pdf-thumbnail">
+										<img src="<?php echo homepage_url() ?>dist/images/parents-world-sep-oct2015.png" class="img-responsive">
+									</div>
+									<div class="pdf-title">
+										Parents World September-October 2015											
+									</div>
+								</div>
+							</a>
+						</div>
+						<div class="col-lg-4 col-md-6 col-sm-6">
+							<a href="<?php echo homepage_url() ?>pdf/motherhood-oct15-2015.pdf" target="_blank">
+								<div class="pdf-item fix-height" data-fix-height-group="pdf-group" style="height: 386px;">
+									<div class="pdf-thumbnail">
+										<img src="<?php echo homepage_url() ?>dist/images/motherhood-oct15-2015.png" class="img-responsive">
+									</div>
+									<div class="pdf-title">
+										Motherhood October 2015											
+									</div>
+								</div>
+							</a>
+						</div>
+						<div class="col-lg-4 col-md-6 col-sm-6">
+							<a href="<?php echo homepage_url() ?>pdf/simply-her-jul2015.pdf" target="_blank">
+								<div class="pdf-item fix-height" data-fix-height-group="pdf-group" style="height: 386px;">
+									<div class="pdf-thumbnail">
+										<img src="<?php echo homepage_url() ?>dist/images/simply-her-jul2015.png" class="img-responsive">
+									</div>
+									<div class="pdf-title">
+										Simply Her July 2015											
+									</div>
+								</div>
+							</a>
+						</div>
+						<div class="col-lg-4 col-md-6 col-sm-6">
+							<a href="<?php echo homepage_url() ?>pdf/parents-world-jul2015.pdf" target="_blank">
+								<div class="pdf-item fix-height" data-fix-height-group="pdf-group" style="height: 386px;">
+									<div class="pdf-thumbnail">
+										<img src="<?php echo homepage_url() ?>dist/images/parents-world-jul2015.png" class="img-responsive">
+									</div>
+									<div class="pdf-title">
+										Parents World July 2015											
+									</div>
+								</div>
+							</a>
+						</div>
+						<div class="col-lg-4 col-md-6 col-sm-6">
+							<a href="<?php echo homepage_url() ?>pdf/may-june-2015-parentsworld.pdf" target="_blank">
+								<div class="pdf-item fix-height" data-fix-height-group="pdf-group" style="height: 386px;">
+									<div class="pdf-thumbnail">
+										<img src="<?php echo homepage_url() ?>dist/images/may-june-2015-parentsworld.png" class="img-responsive">
+									</div>
+									<div class="pdf-title">
+										Parents World May-June 2015											
+									</div>
+								</div>
+							</a>
+						</div>
+						<div class="col-lg-4 col-md-6 col-sm-6">
+							<a href="<?php echo homepage_url() ?>pdf/expat-living-june-2015.pdf" target="_blank">
+								<div class="pdf-item fix-height" data-fix-height-group="pdf-group" style="height: 386px;">
+									<div class="pdf-thumbnail">
+										<img src="<?php echo homepage_url() ?>dist/images/expat-living-june-2015.png" class="img-responsive">
+									</div>
+									<div class="pdf-title">
+										Expat Living June 2015											
+									</div>
+								</div>
+							</a>
+						</div>
+						<div class="col-lg-4 col-md-6 col-sm-6">
+							<a href="<?php echo homepage_url() ?>pdf/motherhood-june-2015.pdf" target="_blank">
+								<div class="pdf-item fix-height" data-fix-height-group="pdf-group" style="height: 386px;">
+									<div class="pdf-thumbnail">
+										<img src="<?php echo homepage_url() ?>dist/images/motherhood-june-2015.png" class="img-responsive">
+									</div>
+									<div class="pdf-title">
+										Motherhood June 2015											
+									</div>
+								</div>
+							</a>
+						</div>
+						
+					</div>
+				</div>
+				<!-- end .pdf-grid -->
+
+			</div>
+
+			<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12 col-md-pull-8">
+				
+				<div class="sidebars">
+
+					<?php include 'template-part/call-button-sidebar.php' ?>
+
+					<?php include 'template-part/contact-us-form-en-sidebar.php' ?>
+
+					<?php include 'template-part/contact-info-en-sidebar.php' ?>
+
+				</div>
+
+			</div>
+
+		</div>
+	</div>
+<!-- END PAGE CONTENT CONTAINER -->
+
+
+<?php include 'footer.php' ?>
